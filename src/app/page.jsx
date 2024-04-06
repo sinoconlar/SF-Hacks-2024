d94c103491bdb0254985ba55766c8fd508a45087
Richard Yip <rdyip892@gmail.com>
2024-04-06 15:23:06 -0700

    Fix home page not rendering

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-
 import GoogleMaps from "./components/GoogleMaps";
 import { readPost } from "../lib/actions";
 import PostComponent from "./components/PostComponent";
@@ -6,21 +5,23 @@ import PostComponent from "./components/PostComponent";
 export default async function Home() {
   const data = await readPost();
   return (
- <GoogleMaps />
-    <div className="min-h-screen flex flex-col gap-5 items-center py-14">
-      {data?.map((post, index) => {
-        return (
-          <PostComponent
-            key={index}
-            id={post.id}
-            title={post.title}
-            username={post.username}
-            location={post.location}
-            description={post.description}
-            showFullPage={false}
-          ></PostComponent>
-        );
-      })}
+    <div>
+      <GoogleMaps />
+      <div className="min-h-screen flex flex-col gap-5 items-center py-14">
+        {data?.map((post, index) => {
+          return (
+            <PostComponent
+              key={index}
+              id={post.id}
+              title={post.title}
+              username={post.username}
+              location={post.location}
+              description={post.description}
+              showFullPage={false}
+            ></PostComponent>
+          );
+        })}
+      </div>
     </div>
   );
 }
